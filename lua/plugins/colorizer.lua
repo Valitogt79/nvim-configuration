5dfe036465f38a13bd7ab604eaa193d2ed3f0403
Oswaldo Alonzo <oalonzo@gmail.com>
2024-06-17 13:46:57 -0600

    Agregando Pluggins: Cmp, colorizer, lsp-config, telescope, which-key

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,13 +1,18 @@
 return {
-    'norcalli/nvim-colorizer.lua',
-    config = function()
-        require'colorizer'.setup({
-          css = {
-            css = false,
-            css_fn = false;
-          },
-          'javascript',
-          'html',
-        })
-    end
+  "norcalli/nvim-colorizer.lua",
+  config = function()
+    require("colorizer").setup({
+      css = {
+        css = false,
+        css_fn = false,
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        names = true,
+        rgb_fn = true,
+        hsl_fn = true,
+      },
+      "javascript",
+      "html",
+    })
+  end,
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,22 +1,23 @@
 return {
     "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      mode = "n", -- NORMAL mode
-  -- prefix: use "<leader>f" for example for mapping everything related to finding files
-  -- the prefix is prepended to every mapping part of `mappings`
-  prefix = "",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
-  noremap = true, -- use `noremap` when creating keymaps
-  nowait = false, -- use `nowait` when creating keymaps
-  expr = false, -- use `expr` when creating keymaps
-    }
-}
+    event = "VimEnter",
+    config = function()
+    require('which-key').setup()
+
+      -- Document existing key chains
+      require('which-key').register {
+        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      }
+      -- visual mode
+      require('which-key').register({
+        ['<leader>h'] = { 'Git [H]unk' },
+      }, { mode = 'v' })
+    end
+  }
+
